425bef50256e415322d082a6170502b52fa66160
nictru <nictru32@gmail.com>
2022-09-14 13:42:13 +0200

    Migrate build into run

--- run.sh
+++ run.sh
@@ -7,4 +7,8 @@ RUNMODE="genomeGenerate"
 FASTAS=$4
 GTF=$5
 
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+bash "$parent_path"/build.sh
+
 docker run -v "$INPUT_DIR":/srv/input -v "$OUTPUT_DIR":/srv/output star_$(whoami) ./STAR --runThreadN "$N_THREADS" --runMode "$RUNMODE" --genomeDir /srv/output --genomeFastaFiles /srv/input/"$FASTAS" --sjdbGTFfile /srv/input/"$GTF"
